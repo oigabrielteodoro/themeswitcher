908d9f43b72d766a36f55f882e8661fbc6857a48
= <zgabrielcodes@gmail.com>
2020-02-28 15:40:31 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
 import React from 'react';
-import { ToastContainer } from 'react-toastify';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Provider } from 'react-redux';
-import { Router } from 'react-router-dom';
-
-import './config/ReactotronConfig';
-
-import Routes from './routes';
-import history from './services/history';
-
-import { store, persistor } from './store';
-
-import GlobalStyle from './styles/global';
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <Router history={history}>
-          <Routes />
-          <GlobalStyle />
-          <ToastContainer autoClose={3000} />
-        </Router>
-      </PersistGate>
-    </Provider>
-  );
+    <div className="App">
+
+    </div>
+  )
 }
