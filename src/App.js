721138c0327b3307cdff7219547b1fc83674feeb
= <zgabrielcodes@gmail.com>
2020-03-24 19:00:42 -0300

    ThemeSwitcher completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { ThemeProvider } from 'styled-components';
+import usePersistedState from './utils/usePersistedState';
+
+import light from './styles/themes/light';
+import dark from './styles/themes/dark';
+
+import Header from './components/Header';
+import GlobalStyle from './styles/global';
 
 export default function App() {
-  return (
-    <div className="App">
+  const [theme, setTheme] = usePersistedState('theme', light);
 
-    </div>
-  )
+  const toggleTheme = useCallback(() => {
+    setTheme(theme.title === 'light' ? dark : light);
+  }, [theme]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Header toggleTheme={toggleTheme} />
+    </ThemeProvider>
+  );
 }
